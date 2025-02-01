1780cf6de858b7070d8aa307c106834fd2b6f21f
DevAbdulAlimRony <alim15@cse.pstu.ac.bd>
2025-02-01 23:08:10 +0600

    banner and trending section

--- tailwind.js
+++ tailwind.js
@@ -3,11 +3,12 @@ tailwind.config = {
         extend: {
             colors: {
                 main: '#FFDC26',
+                secondary: '#F9DF8A',
                 light: '#FFF9F4',
                 para: '#111111',
             },
             fontFamily: {
-                main: 'ADLaM Display',
+                title: 'ADLaM Display',
                 para: 'sora',
             },
             backgroundImage: {
