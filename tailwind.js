11ca5a16f1ad0e35a31edc0e5cb43f2820fb1531
DevAbdulAlimRony <alim15@cse.pstu.ac.bd>
2025-02-01 17:29:25 +0600

    project initialized

---
+++ tailwind.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                main: 'green'
+            },
+            fontFamily: {
+                poppins: "Poppins", serif,
+            },
+            backgroundImage: {
+
+            }
+        }
+    }
+}
